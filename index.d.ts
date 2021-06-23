3964099ee9e79ea84ac226f97d438fdb57651832
mmckenna <mmckenna.phila@gmail.com>
2021-06-23 12:59:31 -0400

    updates gitignore

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,14 @@
 declare module 'my-package/index' {
   export * from 'my-package/something/index';
+
 }
 declare module 'my-package/something/index' {
   export * from 'my-package/something/something';
+
 }
 declare module 'my-package/something/something' {
   export const doSomething: (text: string) => string;
+
 }
 declare module 'my-package' {
   import main = require('my-package/index');
